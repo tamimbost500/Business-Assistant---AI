5742771a34293d412baf4d756f539bb6c9b601b0
Si Tamim <auxiliaryprogrammer@gmail.com>
2024-01-19 17:31:25 +0600

    15 pages complete

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// $(".toggle-password").click(function() {
+
+//     $(this).toggleClass("fa-eye fa-eye-slash");
+//     var input = $($(this).attr("toggle"));
+//     if (input.attr("type") == "password") {
+//       input.attr("type", "text");
+//     } else {
+//       input.attr("type", "password");
+//     }
+//   });
+
+
+// $( "section .avatar-background .background-avatar-wrapper .avatar-bg-1" ).on( "click", function() {
+//     $(this).addClass("checkedd");
+// });
+// $( "section .avatar-background .background-avatar-wrapper .avatar-bg-2" ).on( "click", function() {
+//     $(this).addClass("checkedd");
+//     $("section .avatar-background .background-avatar-wrapper span").removeClass("checkedd");
+// });
